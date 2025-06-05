c36c277227968f3201e573215a29ed3c2a001d59
Jessica45-del <j.ibada-nwafor@se24.qmul.ac.uk>
2025-06-05 12:05:39 +0100

    Updated cli.py and toml file

--- src/multi_agent_system/agents/grounding/grounding_agent.py
+++ src/multi_agent_system/agents/grounding/grounding_agent.py
@@ -5,28 +5,53 @@ diagnostic reasoning agent
 """
 from pydantic_ai import Agent
 from pydantic_ai.models.openai import OpenAIModel
+from pydantic_ai.providers.deepseek import DeepSeekProvider
 from pydantic_ai.providers.openai import OpenAIProvider
+
+
+model = OpenAIModel(
+    model_name="deepseek-chat",
+    provider=DeepSeekProvider()
+)
 
 GROUNDING_SYSTEM_PROMPT(
     "You are an expert in rare disease knowledge."
-    "Your task is to retrieve information about the disease from:"
-    "1) The Monarch KB"
-    "2) and Map MONDO ID to HPO terms."
-    "The recommended workflow is to first...."
-    "Use the Monarch Knowledge Graph to find relevant disease information."
-    "For each disease candidate, collect all associated phenotypes."
-    "Provide a detailed analysis of each disease candidate, including ID, label, and phenotypes."
-    "You will a."
+    "Your task is to provide additional ontological information about rare disease"
+    "You will perform this task by completing the following steps:"
+    "1) Map HPO ID (terms) to MONDO disease IDs"
+    "2) Retrieve disease knowledge using MONDO IDs "
+    "You will map the HPO ID (terms) to MONDO IDs by using the find_mondo_disease_id function"
+    "and you will return as a list in JSON format"
+    "You will retrieve disease knowledge using MONDO IDs by using the get_disease_profile function"
+    "and you will return as a list in JSON format"
+    ""
 )
 
 
 
-
+# Create grounding agent
 grounding_agent = Agent(
-    model ="g",
-    deps_type = GroundingDependencies,
-    result_type=str, # or JSON?
-    system_prompt=GROUNDING_SYSTEM_PROMPT
+   model= model,
+   system_prompt=GROUNDING_SYSTEM_PROMPT
 )
+
+# Register tools
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
